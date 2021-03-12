e1e7628368afc59aeb6b210101db897eba25cec3
byu224 <nadyaawibowo@gmail.com>
2021-03-12 14:14:22 +0700

    Revisi tugas Nadya

--- switchcase.js
+++ switchcase.js
@@ -1,17 +1,20 @@
-let tanggal = 31
-let bulan = 3
-let tahun = 1996
+let tanggal = 30
+let bulan = 4
+let tahun = 'tah'
 
 if(
-    (bulan == 1,3,5,7,8,10,12 && tanggal>31)||
-    (bulan == 4, 6, 9, 11 && tanggal>30) || 
-    (tahun%4==0 && bulan == 2 && tanggal>29) || 
-    (tahun%4 != 0 && bulan == 2 && tanggal>28)
+    ((bulan === 1 || bulan === 3|| bulan === 5|| bulan === 7 ||bulan === 8||bulan === 10||bulan === 12) && tanggal>31)||
+    ((bulan === 4 || bulan === 6|| bulan === 9|| bulan === 11)  && tanggal>30) || 
+    (tahun%4==0 && bulan === 2 && tanggal>29) || 
+    (tahun%4 != 0 && bulan === 2 && tanggal>28)
     ) {
     console.log('masukkan tanggal yang benar!')
     }
 else if (tahun==1996&&bulan==7&&tanggal==15){
     console.log('HAPPY BIRTHDAY!!!!')
+}
+else if (isNaN(tahun)){
+    console.log('masukkan tahun dengar format angka!')
 }
 else{
     switch (bulan) {
@@ -51,5 +54,7 @@ else{
         case 12 :
             console.log(`${tanggal} Desember ${tahun}`);
             break;
+        default :
+            console.log('masukkan bulan yang benar!')
     }
 }
